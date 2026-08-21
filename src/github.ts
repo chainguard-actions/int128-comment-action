e04b7be9037e4d3b006af752aebf178222bb5cf7
actionchain <actionchain@chainguard.dev>
2026-08-21 18:18:18 +0000

    Update int128/comment-action@v1.73.0

---
+++ src/github.ts
@@ -0,0 +1,25 @@
+import assert from 'node:assert'
+import * as fs from 'node:fs/promises'
+import { Octokit } from '@octokit/action'
+import { retry } from '@octokit/plugin-retry'
+import type { WebhookEvent } from '@octokit/webhooks-types'
+
+export const getOctokit = () => new (Octokit.plugin(retry))()
+
+export type Context = {
+  sha: string
+  payload: WebhookEvent
+}
+
+export const getContext = async (): Promise<Context> => {
+  // https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/store-information-in-variables#default-environment-variables
+  return {
+    sha: getEnv('GITHUB_SHA'),
+    payload: JSON.parse(await fs.readFile(getEnv('GITHUB_EVENT_PATH'), 'utf-8')) as WebhookEvent,
+  }
+}
+
+const getEnv = (name: string): string => {
+  assert(process.env[name], `${name} is required`)
+  return process.env[name]
+}
---
+++ src/run.ts
@@ -0,0 +1,39 @@
+import type { Octokit } from '@octokit/action'
+import { runCommand } from './command.js'
+import { postComment, type UpdateIfExistsType } from './comment.js'
+import type { Context } from './github.js'
+
+type Inputs = {
+  post: string
+  updateIfExists: UpdateIfExistsType
+  updateIfExistsKey: string
+  run: string
+  postOnSuccess: string
+  postOnFailure: string
+  repository: string
+  issueNumber: number | undefined
+}
+
+export const run = async (inputs: Inputs, octokit: Octokit, context: Context): Promise<void> => {
+  if (!inputs.post && !inputs.run) {
+    throw new Error(`either post or run must be set`)
+  }
+
+  if (inputs.post) {
+    return await postComment(
+      {
+        body: inputs.post,
+        updateIfExists: inputs.updateIfExists,
+        updateIfExistsKey: inputs.updateIfExistsKey,
+        repository: inputs.repository,
+        issueNumber: inputs.issueNumber,
+      },
+      octokit,
+      context,
+    )
+  }
+
+  if (inputs.run) {
+    return await runCommand(inputs, octokit, context)
+  }
+}
